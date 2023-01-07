bca02ff7169fc29da62a94751f6f0c62b5212cf7
Prashant Chauhan <48925267+prashantnaglaksh@users.noreply.github.com>
2023-01-07 11:48:04 +0530

    test

--- smartContactManager/src/main/java/com/smart/SmartContactManagerApplication.java
+++ smartContactManager/src/main/java/com/smart/SmartContactManagerApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SmartContactManagerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SmartContactManagerApplication.class, args);
+	public static void main(String[] arg) {
+		SpringApplication.run(SmartContactManagerApplication.class, arg);
 	}
 
 }
