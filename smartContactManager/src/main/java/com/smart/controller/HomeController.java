ba5a056f477d8b02a0fe1875e5dcd23ac05421a4
Prashant Chauhan <48925267+prashantnaglaksh@users.noreply.github.com>
2023-01-08 23:19:46 +0530

    saving registration form to database

--- smartContactManager/src/main/java/com/smart/controller/HomeController.java
+++ smartContactManager/src/main/java/com/smart/controller/HomeController.java
@@ -1,15 +1,21 @@
 package com.smart.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.smart.dao.UserRepository;
 import com.smart.entity.User;
+import com.smart.helper.Message;
 
 @Controller
 public class HomeController {
@@ -31,7 +37,32 @@ public class HomeController {
 	@RequestMapping("/signup")
 	public String signup(Model model) {
 		model.addAttribute("title","Signup - Smart Contact Manager");
+		model.addAttribute("user", new User());
 		return "signup";
+	}
+	
+	//handler for registering user
+	@PostMapping("/do_register")
+	public String registerUser (@ModelAttribute("user") User user,
+			@RequestParam(value="agreement", defaultValue = "false") boolean agreement, Model model, HttpSession session){
+		try {
+			if(!agreement)
+				throw new Exception("You Have Not Agreed Terms And Condition");
+			user.setRole("Role_User");
+			user.setEnabled(true);
+			System.out.println("user " + user);
+			System.out.println("agreement " +  agreement);
+			User result = userRepository.save(user);
+			
+			model.addAttribute("user", new User());
+			session.setAttribute("message", new Message("Sussfully Registerd !!", "alert-success"));
+			return "signup";
+		} catch (Exception e) {
+			e.printStackTrace();
+			model.addAttribute("user", user);
+			session.setAttribute("message", new Message("Something Went Wrong !!" + e.getMessage(), "alert-error"));
+			return "signup";
+		}
 	}
 
 }
