48fe5d71d5bdefb6e9011d307868b31ff18b1e23
Prashant Chauhan <48925267+prashantnaglaksh@users.noreply.github.com>
2023-01-24 22:09:20 +0530

    desiigning add contact Form

--- smartContactManager/src/main/java/com/smart/controller/UserController.java
+++ smartContactManager/src/main/java/com/smart/controller/UserController.java
@@ -5,9 +5,12 @@ import java.security.Principal;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.smart.dao.UserRepository;
+import com.smart.entity.Contact;
 import com.smart.entity.User;
 
 @Controller
@@ -17,15 +20,28 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
-	@RequestMapping("/index")
-	public String dashboard(Model model, Principal principal) {
+	//with the help of this annotation this attr is automativcalyy added to all the handelers
+	@ModelAttribute
+	public void addCommanData(Model model, Principal principal) {
 		String userName = principal.getName();
 		System.out.println("i am userName " + userName);
 		// fetching user info using userName i.e, emailId
 		User user = userRepository.getUserByUserName(userName);
 		System.out.println("i am user " + user);
 		model.addAttribute("user", user);
+	}
+	
+	// user dashboard i.e, home handler
+	@RequestMapping("/index")
+	public String dashboard(Model model, Principal principal) {
 		return "user/user_dashboard";
+	}
+	//open add form handeler
+	@GetMapping("/add-contact")
+	public String openAddContactForm(Model model) {
+		model.addAttribute("title", "Add Contact");
+		model.addAttribute("contact", new Contact());
+		return "user/add_contact_form";
 	}
 
 }
