9472c77ff0463ea747462951701f685d19096364
Prashant Chauhan <48925267+prashantnaglaksh@users.noreply.github.com>
2023-01-28 03:28:49 +0530

    paginatsion Change

--- smartContactManager/src/main/java/com/smart/controller/UserController.java
+++ smartContactManager/src/main/java/com/smart/controller/UserController.java
@@ -12,10 +12,14 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -101,16 +105,22 @@ public class UserController {
 		return "user/add_contact_form";
 	}
 	
-	@GetMapping("/show-contacts")
-	public String showContacts(Model model, Principal principal) {
+	//show contact handeler
+	//per page 5[n]
+	//current page = 0[page]
+	@GetMapping("/show-contacts/{page}")
+	public String showContacts(@PathVariable("page") Integer page, Model model, Principal principal) {
 		model.addAttribute("title", "View Contacts");
 		
 		//fetching contacts
 		String userName = principal.getName();
 		User user = this.userRepository.getUserByUserName(userName);
 		int userId = user.getId();
-		List<Contact> contacts = this.contactRepository.findContactByUser(userId);
+		PageRequest pageRequest = PageRequest.of(page, 5);
+		Page<Contact> contacts = this.contactRepository.findContactByUser(userId,pageRequest);
 		model.addAttribute("contacts", contacts);
+		model.addAttribute("currentPage", page);
+		model.addAttribute("totalPages", contacts.getTotalPages());
 		
 		return "user/show_contacts";
 		
