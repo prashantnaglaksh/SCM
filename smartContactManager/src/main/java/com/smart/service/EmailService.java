8ef4699e3bea48e44fee2479fd6742cdd6853385
Prashant Chauhan <48925267+prashantnaglaksh@users.noreply.github.com>
2023-02-03 00:02:48 +0530

    adding email service

---
+++ smartContactManager/src/main/java/com/smart/service/EmailService.java
@@ -0,0 +1,41 @@
+package com.smart.service;
+
+import org.springframework.stereotype.Service;
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+@Service
+public class EmailService {
+    public static boolean sendOTP(String subject, String message, String to) {
+        final String from = "prashantchauhan515@gmail.com";
+        final String password = "fgdigsxzoriyzckd";
+
+        Properties props = new Properties();
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.port", "587");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+
+        Session session = Session.getInstance(props, new javax.mail.Authenticator() {
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(from, password);
+            }
+        });
+
+        try {
+            MimeMessage mimeMessage = new MimeMessage(session);
+            mimeMessage.setFrom(new InternetAddress(from));
+            mimeMessage.addRecipient(Message.RecipientType.TO, new InternetAddress(to));
+            mimeMessage.setSubject(subject);
+            mimeMessage.setText(message);
+
+            Transport.send(mimeMessage);
+            return true;
+        } catch (MessagingException e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+}
