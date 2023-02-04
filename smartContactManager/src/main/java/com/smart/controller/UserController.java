2ecfc42499bc2b30e5b8a89e4d04bcbe99bdf952
Prashant Chauhan <48925267+prashantnaglaksh@users.noreply.github.com>
2023-02-04 14:44:04 +0530

    adding Payment Integration in our project

--- smartContactManager/src/main/java/com/smart/controller/UserController.java
+++ smartContactManager/src/main/java/com/smart/controller/UserController.java
@@ -7,10 +7,12 @@ import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.security.Principal;
 import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 import javax.servlet.http.HttpSession;
 
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.domain.Page;
@@ -23,10 +25,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.razorpay.Order;
+import com.razorpay.RazorpayClient;
 import com.smart.dao.ContactRepository;
 import com.smart.dao.UserRepository;
 import com.smart.entity.Contact;
@@ -241,5 +247,34 @@ public class UserController {
 		
 		return "redirect:/user/index";
 	}
+	
+	// payment handler
+	@PostMapping("/create_order")
+	@ResponseBody
+	public String createOrder(@RequestBody Map<String, Object> data) throws Exception {
+		try {
+			String amount = (String) data.get("amount");
+			int lAmount = Integer.valueOf(amount);
+			System.out.println("i am here ");
+			RazorpayClient client = new RazorpayClient("id", "key");
+			
+			  JSONObject orderRequest = new JSONObject();
+			  orderRequest.put("amount", lAmount*100); // amount in the smallest currency unit
+			  orderRequest.put("currency", "INR");
+			  orderRequest.put("receipt", "order_rcptid_11");
+
+			  Order order = client.Orders.create(orderRequest);
+			  // we should save order details in our db
+			  System.out.println(order);
+			  return order.toString();
+			  
+			} catch (Exception e) {
+			  // Handle Exception
+			  System.out.println(e.getMessage());
+			  return e.getMessage();
+			}
+		//return order.toString();
+	}
+	
 	
 }
--- smartContactManager/src/main/resources/static/js/script.js
+++ smartContactManager/src/main/resources/static/js/script.js
@@ -83,3 +83,82 @@ const searchContact1=()=>{
 		
 	}	
 };
+
+
+
+// first request to server to craete order
+function paymentStart(){
+	console.log("payment started");
+	let amount = $("#payment_field").val();
+	if(amount == '' || amount == null){
+		alert("Please fill");
+		return;
+	}
+	
+	// using AJAX to send request to server o create ORDER
+	$.ajax(
+		{
+			url : "/user/create_order",
+			data:JSON.stringify({amount:amount, info:'order_request'}),
+			contentType:'application/json',
+			type:'POST',
+			dataType:'json',
+			success:function(response){
+				//this function will invoked when success
+				console.log(response);
+				if(response.status == "created"){
+					//open payment form
+					var options = {
+    "key": "YOUR_KEY_ID", // Enter the Key ID generated from the Dashboard
+    "amount": "50000", // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    "currency": "INR",
+    "name": "Smart Contact Manager",
+    "description": "Donation Transaction",
+    "image": "https://example.com/your_logo",
+    "order_id": response.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+    "handler": function (response){
+        alert(response.razorpay_payment_id);
+        alert(response.razorpay_order_id);
+        alert(response.razorpay_signature)
+    },
+    "prefill": {
+        "name": "",
+        "email": "gaurav.kumar@example.com",
+        "contact": ""
+    },
+    "notes": {
+        "address": "Razorpay Corporate Office"
+    },
+    "theme": {
+        "color": "#3399cc"
+    }
+};
+
+var rzp = new Razorpay(options);
+
+rzp.on('payment.failed', function (response){
+        alert(response.error.code);
+        alert(response.error.description);
+        alert(response.error.source);
+        alert(response.error.step);
+        alert(response.error.reason);
+        alert(response.error.metadata.order_id);
+        alert(response.error.metadata.payment_id);
+});
+   
+    rzp.open();
+
+					
+				}
+			},
+			error:function(error){
+				// invoked when error
+				console.log(error);
+				alert("Something wentwrong !!");
+			}
+		}
+	);
+}
+
+
+
